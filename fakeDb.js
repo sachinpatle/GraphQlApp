3582772159310f8e88eb3ac40b79f449f03394e5
sachinpatle <sachinpatle189@gmail.com>
2021-12-07 21:49:07 +0530

    changes

---
+++ fakeDb.js
@@ -0,0 +1,32 @@
+export const users = [{
+
+    id: "11",
+    firstName: "sachin",
+    lastName: "patle",
+    email: "sachinpatle1898@gmailcom",
+    password: "12345"
+},
+{
+    id: "22",
+    firstName: "rahul",
+    lastName: "bisen",
+    email: "rahulbisen1898@gmailcom",
+    password: "12345"
+},
+{
+    id: "33",
+    firstName: "karan",
+    lastName: "pardhi",
+    email: "karanpardhi1898@gmailcom",
+    password: "12345"
+}]
+
+//Quotes of users
+export const quotes = [{
+    name: "do what ever you want",
+    by: "11"
+},
+{
+    name: "if it works don't touch it",
+    by: "22"
+}]
--- server.js
+++ server.js
@@ -1,19 +1,55 @@
-import {ApolloServer,gql} from 'apollo-server';
-import {ApolloServerPluginLandingPageGraphQLPlayground} from 'apollo-server-core';
+import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+import { quotes, users } from './fakeDb.js';
 
 
 //making the Schema. 
 //This is the client queries present in the object type Queries.
+
 const typeDefs = gql`
 type Query{
-    greet:String
-}`
+    User:[users]
+    Quotes:[quotes]
+}
+
+type users{
+    id:ID
+    firstName:String
+    lastName:String
+    email:String
+}
+
+type quotes{
+    name:String
+    by:ID
+}
+`
+
 //Making Resolver
-// This  will give the response of the client query 
-// Here fun is present which gives the response of the schema mapped query
+//This  will give the response of the client query 
+//Here fun is present which gives the response of the schema mapped query
 
 const resolvers = {
-    Query:{
-
+    Query: {
+        User: () => users,
+        Quotes:()=>quotes
     }
+
+   
 }
+
+//Creating the Appollo server
+//and listen to some port there
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [
+        ApolloServerPluginLandingPageGraphQLPlayground()
+    ]
+});
+
+server.listen().then(({ url }) => {
+    console.log("server is ready", url);
+});
+
